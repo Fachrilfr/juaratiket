90e5c22fd32d46942d68306dba99e135ac4bd50e
Fachrilfr <strzack84@gmail.com>
2025-08-11 13:50:02 +0700

    first commit

---
+++ database/migrations/2025_06_23_122616_create_ticket_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_photos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('photo');
+            $table->softDeletes();
+            $table->foreignId('ticket_id')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('price');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_photos');
+    }
+};
